d24587a4d9b973bd8d582c2d787a6183e06611a5
yina69880 <60630442+yina69880@users.noreply.github.com>
2020-02-25 12:13:24 +1300

    Scale factor 'Ifs'
    The code developed in this section helps to address the usability implication. It aims to prevent users from making errors (eg.  trying to scale a recipe by too much or too little) but also gives users freedom to do what they wish to do.

--- 03_find_scale_factor.py
+++ 03_find_scale_factor.py
@@ -20,17 +20,30 @@ def num_check(question):
             if response <= 0:
                 print(Error)
             else:
-
-
                 return response
 
         except ValueError:
             print(Error)
 # Main routine
+sf_ok = "no"
+while sf_ok =="no":
 
-serving_size = num_check("What is the recipe serving size?")
-desired_size = num_check("How many servings are needed? ")
+    serving_size = num_check("What is the recipe serving size?")
+    desired_size = num_check("How many servings are needed? ")
 
-scale_factor = desired_size / serving_size
+    scale_factor = desired_size / serving_size
+
+    if scale_factor < 0.25:
+        sf_ok = input("Warning: This scale is very small "
+                  "and you might struggle to accurately weigh"
+                  "the ingredients. \n"
+                  "Do you want to keep going? (type 'no' to change "
+                  "your desired serving size)")
+    elif scale_factor > 4:
+        sf_ok = input("Warning: This scale is quite large - you might "
+                  "have issues with mixing bowl space / oven space. \n"
+                  "Do you want to keep going? (type 'no' to change"
+                  "your desired serving size)")
+
 
 print("Scale Factor: {}".format(scale_factor))
