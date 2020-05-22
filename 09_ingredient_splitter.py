eba3e0fb19f59b0c7f5ce2516b81ab9747e79b88
yina69880 <60630442+yina69880@users.noreply.github.com>
2020-05-22 14:28:34 +1200

    finalised component 9

--- 09_ingredient_splitter.py
+++ 09_ingredient_splitter.py
@@ -5,8 +5,23 @@ recipe_line = "1 1/2 mL flour"  # change to input statement in due course
 
 mixed_regex = "\d{1,3}\s\d{1,3}\/\d{1,3}"
 
-if re.matched(mixed_regex, recipe_line):
+if re.match(mixed_regex, recipe_line):
     print("true")
-else:
-    print("false")
-    
+    # Get mixed number by matching the regx
+    pre_mixed_num = re.match(mixed_regex, recipe_line)
+    mixed_num = pre_mixed_num.group()
+
+    # Replace space with a + sign
+    amount = mixed_num.replace(" ", "+")
+    # change the string into a decimal
+    amount = eval(amount)
+    print(amount)
+
+    # get unit and ingredient...
+    compile_regex = re.compile(mixed_regex)
+    print(compile_regex)
+    unit_ingredient = re.split(compile_regex, recipe_line)
+
+
+
+
